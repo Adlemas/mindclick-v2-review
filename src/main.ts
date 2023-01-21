afc617a146cff10fb72cf6978401211fc212be5d
AmirAdal <amir.adal@cobold.in>
2023-01-21 23:15:09 +0100

    feat: pipe validation to app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from 'src/config/config.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const configService = app.get(ConfigService);
   await app.listen(configService.getPort());
 }
