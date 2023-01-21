7f857d0c96019d961f39b303ed15facc455dd23a
AmirAdal <amir.adal@cobold.in>
2023-01-21 20:50:11 +0100

    feat: findOne method with mongoose in users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
+import { Model } from 'mongoose';
+import { from, Observable } from 'rxjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  @InjectModel(User.name) private userModel: Model<UserDocument>;
+
+  findOne(email: string): Observable<UserDocument> {
+    return from(this.userModel.findOne({ email }).exec());
+  }
+}
