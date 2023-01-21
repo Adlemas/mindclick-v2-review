d5e0483a7396688c801986177566913e5de86acb
AmirAdal <amir.adal@cobold.in>
2023-01-21 19:53:33 +0100

    feat: users module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from 'src/config/config.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule, AuthModule],
+  imports: [ConfigModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
