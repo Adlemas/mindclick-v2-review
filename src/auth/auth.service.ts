65d86379f1a5f1a2d0c944f813b2c4e501b40302
AmirAdal <amir.adal@cobold.in>
2023-01-21 23:14:58 +0100

    feat: use login dto apart from getting full user data

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { from, Observable, of, switchMap } from 'rxjs';
-import { User, UserDocument } from 'src/schemas/user.schema';
+import { User } from 'src/schemas/user.schema';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { Payload } from 'src/auth/interface/payload.interface';
+import { LoginDto } from 'src/auth/dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -32,18 +33,23 @@ export class AuthService {
             }),
           );
         }
-        return null;
+        throw new NotFoundException('Invalid credentials');
       }),
     );
   }
 
-  login(user: UserDocument) {
-    const payload: Payload = {
-      email: user.email,
-      sub: user._id.toString(),
-    };
-    return {
-      accessToken: this.jwtService.sign(payload),
-    };
+  login(user: LoginDto): Observable<any> {
+    return from(this.validateUser(user.email, user.password)).pipe(
+      switchMap((user) => {
+        if (user) {
+          const payload: Payload = {
+            email: user.email,
+            sub: user._id.toString(),
+          };
+          return of(payload);
+        }
+        throw new NotFoundException('Invalid credentials');
+      }),
+    );
   }
 }
