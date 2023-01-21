a15ebbdbe401955c9038e8284709d1af0c2cd9f6
AmirAdal <amir.adal@cobold.in>
2023-01-21 21:04:06 +0100

    feat: implement validateUser in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { from, Observable, of, switchMap } from 'rxjs';
+import { User } from 'src/schemas/user.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  @Inject(UsersService)
+  private readonly usersService: UsersService;
+
+  validateUser(
+    email: string,
+    pass: string,
+  ): Observable<Omit<User, 'password'>> {
+    return from(this.usersService.findOne(email)).pipe(
+      switchMap((user) => {
+        if (user && user.password === pass) {
+          return from(bcrypt.compare(pass, user.password)).pipe(
+            switchMap((isMatch) => {
+              if (isMatch) {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                const { password, ...result } = user;
+                return of(result);
+              }
+              return of(null);
+            }),
+          );
+        }
+        return null;
+      }),
+    );
+  }
+}
