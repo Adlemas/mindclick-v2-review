b220b838293dff1755c1ece38b61ff837e6bb1f4
AmirAdal <amir.adal@cobold.in>
2023-01-21 21:12:52 +0100

    feat: passport & jwt module integration in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { configService } from 'src/config/config.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: configService.getJwtSecret(),
+      signOptions: { expiresIn: configService.getAccessTokenExpiresIn() },
+    }),
+  ],
   providers: [AuthService],
+  exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
