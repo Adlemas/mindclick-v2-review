30a5eab11785d441740294eecd9bb65b4bd1eddc
AmirAdal <amir.adal@cobold.in>
2023-01-22 11:53:21 +0100

    chore: types provided in locale service

--- src/locale/locale.service.ts
+++ src/locale/locale.service.ts
@@ -2,13 +2,13 @@ import { Inject, Injectable } from '@nestjs/common';
 import { I18nContext, I18nService, Path, TranslateOptions } from 'nestjs-i18n';
 
 @Injectable()
-export class LocaleService {
+export class LocaleService<K = Record<string, unknown>> {
   @Inject(I18nService)
   private readonly i18nService: I18nService;
 
-  translate<P extends Path<string>>(key: P, options?: TranslateOptions) {
+  translate<P extends Path<K> = any>(key: P, options?: TranslateOptions) {
     const i18nContext = I18nContext.current();
-    return this.i18nService.translate('auth.notFound', {
+    return this.i18nService.translate(key, {
       lang: i18nContext.lang ?? undefined,
       ...options,
     });
