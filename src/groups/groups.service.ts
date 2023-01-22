f804eb137a7398a37e119b451cd051dc169ea427
AmirAdal <amir.adal@cobold.in>
2023-01-22 17:21:28 +0100

    feat: created group service with addGroup method

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Observable, switchMap } from 'rxjs';
+import { User } from 'src/schemas/user.schema';
+import { GroupRepository } from 'src/groups/repository/group.repository';
+import { CreateGroupDto } from 'src/groups/dto/create-group.dto';
+import { Group } from 'src/schemas/group.schema';
 
 @Injectable()
-export class GroupsService {}
+export class GroupsService {
+  @Inject(GroupRepository)
+  private readonly groupRepository: GroupRepository;
+
+  addGroup(
+    createGroupDto: CreateGroupDto,
+    user: Observable<User>,
+  ): Observable<Group> {
+    return user.pipe(
+      switchMap((user) => {
+        return this.groupRepository.createGroup(createGroupDto, user._id);
+      }),
+    );
+  }
+}
