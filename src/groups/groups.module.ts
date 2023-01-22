ddddc5ae2b9f7d594b523545f039786e86224cfc
AmirAdal <amir.adal@cobold.in>
2023-01-22 17:21:15 +0100

    feat: import mongoose for feature & group repository

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { GroupsController } from './groups.controller';
+import { GroupRepository } from 'src/groups/repository/group.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Group, GroupSchema } from 'src/schemas/group.schema';
 
 @Module({
-  providers: [GroupsService],
+  imports: [
+    MongooseModule.forFeature([{ name: Group.name, schema: GroupSchema }]),
+  ],
+  providers: [GroupsService, GroupRepository],
   controllers: [GroupsController],
 })
 export class GroupsModule {}
