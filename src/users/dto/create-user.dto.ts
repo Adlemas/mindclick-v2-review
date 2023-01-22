b59f10389ec3ec82cd6e5a3d1e2ff50f1c325684
AmirAdal <amir.adal@cobold.in>
2023-01-22 23:12:51 +0100

    feat: create user dto provided

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,48 @@
+import {
+  IsDate,
+  IsEmail,
+  IsNumber,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
+import { Type } from 'class-transformer';
+import { Schema } from 'mongoose';
+
+export class CreateUserDto {
+  @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
+  firstName: string;
+
+  @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
+  lastName: string;
+
+  @IsEmail({}, { message: i18nValidationMessage('validation.NOT_EMAIL') })
+  email: string;
+
+  @IsPhoneNumber(undefined, {
+    message: i18nValidationMessage('validation.NOT_PHONE_NUMBER'),
+  })
+  phone: string;
+
+  @Type(() => Date)
+  @IsDate({ message: i18nValidationMessage('validation.NOT_DATE') })
+  birthDate: Date;
+
+  @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
+  description: string;
+
+  @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
+  city: string;
+
+  @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
+  address: string;
+
+  @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
+  groupId: Schema.Types.ObjectId;
+
+  @IsNumber({}, { message: i18nValidationMessage('validation.NOT_NUMBER') })
+  rate: number;
+
+  @IsNumber({}, { message: i18nValidationMessage('validation.NOT_NUMBER') })
+  points: number;
+}
