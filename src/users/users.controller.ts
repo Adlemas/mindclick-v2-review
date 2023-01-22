9d00534fab5c7b31d02165a8384d0c536c01ddac
AmirAdal <amir.adal@cobold.in>
2023-01-22 16:08:36 +0100

    fix: correct endpoints for update user & get my profile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,26 @@
-import { Body, Controller, Inject, Put, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { UpdateUserDto } from 'src/users/dto/update-user.dto';
 import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
 import { Observable } from 'rxjs';
 import { User } from 'src/schemas/user.schema';
 
-@Controller('users')
+@Controller()
 export class UsersController {
   @Inject(UsersService)
   private readonly usersService: UsersService;
 
   @UseGuards(AccessTokenGuard)
-  @Put()
-  async update(@Req() req, @Body() dto: UpdateUserDto) {
+  @Put('users')
+  update(@Req() req, @Body() dto: UpdateUserDto) {
     const user = req.user as Observable<User>;
     return this.usersService.update(user, {
       firstName: dto.firstName,
@@ -23,5 +31,11 @@ export class UsersController {
       description: dto.description,
       phone: dto.phone,
     });
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Get('myprofile')
+  getProfile(@Req() req) {
+    return req.user;
   }
 }
