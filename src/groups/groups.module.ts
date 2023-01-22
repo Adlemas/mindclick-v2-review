0ee5873c7a82e2d28739d077d33854cfe50c16ca
AmirAdal <amir.adal@cobold.in>
2023-01-23 00:29:09 +0100

    feat: import users module

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { GroupsController } from './groups.controller';
 import { GroupRepository } from 'src/groups/repository/group.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Group, GroupSchema } from 'src/schemas/group.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Group.name, schema: GroupSchema }]),
+    forwardRef(() => UsersModule),
   ],
   providers: [GroupsService, GroupRepository],
   controllers: [GroupsController],
