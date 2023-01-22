e83a0d2b992ca3c0ee2330e5cb3700def8eefb01
AmirAdal <amir.adal@cobold.in>
2023-01-22 18:17:48 +0100

    feat: getMyGroups & updateGroup endpoints created

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { GroupsService } from 'src/groups/groups.service';
 import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
 import { RolesGuard } from 'src/users/guard/roles.guard';
 import { Roles } from 'src/users/decorator/roles.decorator';
 import { Role } from 'src/enum/role.enum';
 import { CreateGroupDto } from 'src/groups/dto/create-group.dto';
+import { UpdateGroupDto } from 'src/groups/dto/update-group.dto';
 
 @Controller('groups')
 export class GroupsController {
@@ -16,5 +26,19 @@ export class GroupsController {
   @Post()
   addGroup(@Req() req, @Body() createGroupDto: CreateGroupDto) {
     return this.groupsService.addGroup(createGroupDto, req.user);
+  }
+
+  @Roles(Role.TEACHER)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Get()
+  getMyGroups(@Req() req) {
+    return this.groupsService.getUserGroups(req.user);
+  }
+
+  @Roles(Role.TEACHER)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Put()
+  updateGroup(@Req() req, @Body() updateGroupDto: UpdateGroupDto) {
+    return this.groupsService.updateGroupByOwner(req.user, updateGroupDto);
   }
 }
