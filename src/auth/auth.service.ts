8035bc9a919866b3260ec2689b6832f26b019aa1
AmirAdal <amir.adal@cobold.in>
2023-01-22 13:04:02 +0100

    feat: generate refresh token on login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,19 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { from, Observable, of, switchMap } from 'rxjs';
 import { User } from 'src/schemas/user.schema';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
-import { Payload } from 'src/auth/interface/payload.interface';
 import { LoginDto } from 'src/auth/dto/login.dto';
 import { LocaleService } from 'src/locale/locale.service';
+import { Tokens } from 'src/auth/interface/tokens.interface';
+import { Promise } from 'mongoose';
+import { configService } from 'src/config/config.service';
 
 @Injectable()
 export class AuthService {
@@ -33,7 +40,7 @@ export class AuthService {
                 const { password, ...result } = user.toObject();
                 return of(result);
               }
-              return of(null);
+              throw new BadRequestException('auth.incPass');
             }),
           );
         }
@@ -44,14 +51,62 @@ export class AuthService {
     );
   }
 
-  login(user: LoginDto): Observable<any> {
+  login(user: LoginDto): Observable<Tokens> {
     return from(this.validateUser(user.email, user.password)).pipe(
       switchMap((user) => {
-        const payload: Payload = {
-          email: user.email,
-          sub: user._id.toString(),
-        };
-        return of({ accessToken: this.jwtService.sign(payload) });
+        return this.getTokens(user._id, user.email).pipe(
+          switchMap((tokens) => {
+            return this.updateRefreshToken(user._id, tokens.refreshToken).pipe(
+              switchMap(() => {
+                return of(tokens);
+              }),
+            );
+          }),
+        );
+      }),
+    );
+  }
+
+  updateRefreshToken(userId: string, refreshToken: string) {
+    return from(bcrypt.hash(refreshToken, 10)).pipe(
+      switchMap((hash) => {
+        return from(
+          this.usersService.updateOne(userId, { refreshToken: hash }),
+        );
+      }),
+    );
+  }
+
+  getTokens(userId: string, email: string): Observable<Tokens> {
+    return from(
+      Promise.all([
+        this.jwtService.signAsync(
+          {
+            sub: userId,
+            email,
+          },
+          {
+            secret: configService.getJwtSecret(),
+            expiresIn: configService.getAccessTokenExpiresIn(),
+          },
+        ),
+        this.jwtService.signAsync(
+          {
+            sub: userId,
+            email,
+          },
+          {
+            secret: configService.getRefreshJwtSecret(),
+            expiresIn: configService.getRefreshTokenExpiresIn(),
+          },
+        ),
+      ]),
+    ).pipe(
+      switchMap(([accessToken, refreshToken]) => {
+        return of({
+          accessToken,
+          refreshToken,
+        });
       }),
     );
   }
