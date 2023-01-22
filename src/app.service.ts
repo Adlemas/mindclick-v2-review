61742c0b72837603e9e8f493e71581e99c03a215
AmirAdal <amir.adal@cobold.in>
2023-01-22 14:49:30 +0100

    feat: provided getProfile method in app service for getting user's profile

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/schemas/user.schema';
+import { from, Observable, of, switchMap } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getProfile(user: User | Observable<User>): Observable<User> {
+    if (user instanceof Observable) {
+      return from(user).pipe(
+        switchMap((user) => {
+          if (user) {
+            return of(user);
+          }
+          throw new Error('User not found');
+        }),
+      );
+    }
+    return of(user);
   }
 }
