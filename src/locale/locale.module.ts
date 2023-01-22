d5494ec10d5a613019df94fee9c2f50fe0ad8cff
AmirAdal <amir.adal@cobold.in>
2023-01-22 11:52:05 +0100

    feat: import i18n module in locale global module

--- src/locale/locale.module.ts
+++ src/locale/locale.module.ts
@@ -1,8 +1,26 @@
 import { Global, Module } from '@nestjs/common';
 import { LocaleService } from './locale.service';
+import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
+import { ConfigService } from 'src/config/config.service';
+import { UserLocaleResolver } from 'src/lib/resolvers/user-locale.resolver';
+import * as path from 'path';
 
 @Global()
 @Module({
+  imports: [
+    I18nModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        fallbackLanguage: configService.getLang(),
+        loaderOptions: {
+          path: path.join(__dirname, '..', `/i18n/`),
+          watch: configService.isDev(),
+        },
+      }),
+      resolvers: [AcceptLanguageResolver, UserLocaleResolver],
+      inject: [ConfigService],
+    }),
+  ],
   providers: [LocaleService],
+  exports: [LocaleService],
 })
 export class LocaleModule {}
