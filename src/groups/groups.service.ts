7f88020250247c38fd07ed076cd0f1ec32aa2906
AmirAdal <amir.adal@cobold.in>
2023-01-22 18:18:04 +0100

    feat: provided methods for getting user groups & updating them

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -4,6 +4,7 @@ import { User } from 'src/schemas/user.schema';
 import { GroupRepository } from 'src/groups/repository/group.repository';
 import { CreateGroupDto } from 'src/groups/dto/create-group.dto';
 import { Group } from 'src/schemas/group.schema';
+import { UpdateGroupDto } from 'src/groups/dto/update-group.dto';
 
 @Injectable()
 export class GroupsService {
@@ -17,6 +18,25 @@ export class GroupsService {
     return user.pipe(
       switchMap((user) => {
         return this.groupRepository.createGroup(createGroupDto, user._id);
+      }),
+    );
+  }
+
+  getUserGroups(user: Observable<User>) {
+    return user.pipe(
+      switchMap((user) => {
+        return this.groupRepository.getUserGroups(user._id);
+      }),
+    );
+  }
+
+  updateGroupByOwner(user: Observable<User>, updateGroupDto: UpdateGroupDto) {
+    return user.pipe(
+      switchMap((user) => {
+        return this.groupRepository.updateGroupByOwner(
+          user._id,
+          updateGroupDto,
+        );
       }),
     );
   }
