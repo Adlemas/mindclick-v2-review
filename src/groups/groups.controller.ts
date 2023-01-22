37cf5091a789d036b6367f550bae4fcd8c5c250e
AmirAdal <amir.adal@cobold.in>
2023-01-22 17:21:00 +0100

    feat: created endpoint for creating group in group controller

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import { GroupsService } from 'src/groups/groups.service';
+import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
+import { RolesGuard } from 'src/users/guard/roles.guard';
+import { Roles } from 'src/users/decorator/roles.decorator';
+import { Role } from 'src/enum/role.enum';
+import { CreateGroupDto } from 'src/groups/dto/create-group.dto';
 
 @Controller('groups')
-export class GroupsController {}
+export class GroupsController {
+  @Inject(GroupsService)
+  private readonly groupsService: GroupsService;
+
+  @Roles(Role.TEACHER)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Post()
+  addGroup(@Req() req, @Body() createGroupDto: CreateGroupDto) {
+    return this.groupsService.addGroup(createGroupDto, req.user);
+  }
+}
