7a43d1caaeab1a72dd3a05de0ca43efc00e6d5a1
AmirAdal <amir.adal@cobold.in>
2023-01-22 13:53:39 +0100

    feat: use global filters & pipes from i18n

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from 'src/config/config.service';
 import { ValidationPipe } from '@nestjs/common';
+import { I18nValidationPipe, I18nValidationExceptionFilter } from 'nestjs-i18n';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(
+    new I18nValidationExceptionFilter({
+      detailedErrors: false,
+    }),
+  );
+
+  app.useGlobalPipes(
+    new I18nValidationPipe({
+      transform: true,
+    }),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
