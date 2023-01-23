7fb88c49b95d464b0115ac6c792c94118ae6c6b9
AmirAdal <amir.adal@cobold.in>
2023-01-23 20:51:47 +0100

    feat(service): get my rank method provided

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,13 @@
 import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { User } from 'src/schemas/user.schema';
-import { from, Observable, switchMap } from 'rxjs';
+import { from, Observable, of, switchMap } from 'rxjs';
 import { UpdateUserDto } from 'src/users/dto/update-user.dto';
 import { UserRepository } from 'src/users/repository/user.repository';
 import { isValidObjectId, Schema } from 'mongoose';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { LocaleService } from 'src/locale/locale.service';
+import { Role } from 'src/enum/role.enum';
+import { Rank } from 'src/users/interface/rank.interface';
 
 @Injectable()
 export class UsersService {
@@ -117,6 +119,36 @@ export class UsersService {
     return user.pipe(
       switchMap((u) => {
         return this.userRepository.createUser(u._id, dto);
+      }),
+    );
+  }
+
+  getMyRank(user: Observable<User>): Observable<Rank> {
+    return user.pipe(
+      switchMap((user) => {
+        if (user.role === Role.STUDENT) {
+          return of({
+            rate: user.rate,
+            points: user.points,
+          });
+        }
+        if (user.role === Role.TEACHER) {
+          return this.userRepository.getUserGroups(user._id).pipe(
+            switchMap((groups) => {
+              return from(this.userRepository.getGroupsRank(groups)).pipe(
+                switchMap((rates) => {
+                  return of({
+                    rate: rates.reduce((sum, curr) => sum + curr.rate, 0),
+                    points: rates.reduce((sum, curr) => sum + curr.points, 0),
+                  });
+                }),
+              );
+            }),
+          );
+        }
+        throw new ForbiddenException(
+          this.localeService.translate('errors.forbidden'),
+        );
       }),
     );
   }
