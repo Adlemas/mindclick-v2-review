3689cb4e885cc8b0044486c1b847c10db0170eb7
AmirAdal <amir.adal@cobold.in>
2023-01-23 22:45:53 +0100

    feat: mental generate method in expression service

--- src/expression/expression.service.ts
+++ src/expression/expression.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { MentalPayloadDto } from 'src/expression/dto/mental-payload.dto';
+import expression from 'src/lib/expression';
 
 @Injectable()
-export class ExpressionService {}
+export class ExpressionService {
+  mental(payload: MentalPayloadDto): number[] {
+    const { formula, terms, min, max, isBiggerMax } = payload;
+    return expression(formula, terms, min, max, isBiggerMax);
+  }
+}
