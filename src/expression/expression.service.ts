75b957788cafc5b1abbe6e8ff4fd740ce2f3c860
AmirAdal <amir.adal@cobold.in>
2023-01-23 23:09:11 +0100

    feat: divide service function

--- src/expression/expression.service.ts
+++ src/expression/expression.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+
 import { MentalPayloadDto } from 'src/expression/dto/mental-payload.dto';
 import expression from 'src/lib/expression';
+
 import { MultiplyPayloadDto } from 'src/expression/dto/mutliply-payload.dto';
 import { MultiplyResponse } from 'src/expression/interface/multiply-response.interface';
 import multiply from 'src/lib/multiply';
+
+import { DividePayloadDto } from 'src/expression/dto/divide-payload.dto';
+import { DivideResponse } from 'src/expression/interface/divide-response.interface';
+import divide from 'src/lib/divide';
 
 @Injectable()
 export class ExpressionService {
@@ -15,5 +21,10 @@ export class ExpressionService {
   multiply(payload: MultiplyPayloadDto): MultiplyResponse {
     const { first, second } = payload;
     return multiply(first[0].length, second[0].length, first, second);
+  }
+
+  divide(payload: DividePayloadDto): DivideResponse {
+    const { first, second, remainder } = payload;
+    return divide(first, second, { remainder: remainder ?? 0 });
   }
 }
