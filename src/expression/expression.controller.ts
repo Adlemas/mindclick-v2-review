5b7797c08b5116ff2088e7cd531a2c80f31ff61a
AmirAdal <amir.adal@cobold.in>
2023-01-23 22:49:19 +0100

    feat: mental endpoint in expression controller provided

--- src/expression/expression.controller.ts
+++ src/expression/expression.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Inject } from '@nestjs/common';
+import { ExpressionService } from 'src/expression/expression.service';
+import { MentalPayloadDto } from 'src/expression/dto/mental-payload.dto';
 
 @Controller('expression')
-export class ExpressionController {}
+export class ExpressionController {
+  @Inject(ExpressionService)
+  private readonly expressionService: ExpressionService;
+
+  @Get('mental')
+  mental(@Body() dto: MentalPayloadDto) {
+    return this.expressionService.mental(dto);
+  }
+}
