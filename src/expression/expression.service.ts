c7829391415c503fdfa41e14d9c795183b288009
AmirAdal <amir.adal@cobold.in>
2023-01-23 23:02:04 +0100

    feat: multiply generate service method

--- src/expression/expression.service.ts
+++ src/expression/expression.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { MentalPayloadDto } from 'src/expression/dto/mental-payload.dto';
 import expression from 'src/lib/expression';
+import { MultiplyPayloadDto } from 'src/expression/dto/mutliply-payload.dto';
+import { MultiplyResponse } from 'src/expression/interface/multiply-response.interface';
+import multiply from 'src/lib/multiply';
 
 @Injectable()
 export class ExpressionService {
   mental(payload: MentalPayloadDto): number[] {
     const { formula, terms, min, max, isBiggerMax } = payload;
     return expression(formula, terms, min, max, isBiggerMax);
+  }
+
+  multiply(payload: MultiplyPayloadDto): MultiplyResponse {
+    const { first, second } = payload;
+    return multiply(first[0].length, second[0].length, first, second);
   }
 }
