3f13613d94c24f583892da2393a6752c46b41e8c
AmirAdal <amir.adal@cobold.in>
2023-01-23 22:54:06 +0100

    feat: protect route with access token guard

--- src/expression/expression.controller.ts
+++ src/expression/expression.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, Inject } from '@nestjs/common';
+import { Body, Controller, Get, Inject, UseGuards } from '@nestjs/common';
 import { ExpressionService } from 'src/expression/expression.service';
 import { MentalPayloadDto } from 'src/expression/dto/mental-payload.dto';
+import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
 
 @Controller('expression')
 export class ExpressionController {
   @Inject(ExpressionService)
   private readonly expressionService: ExpressionService;
 
+  @UseGuards(AccessTokenGuard)
   @Get('mental')
   mental(@Body() dto: MentalPayloadDto) {
     return this.expressionService.mental({
