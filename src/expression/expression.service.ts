b91460e6ebd571ffd7344281d25c209ef8459d62
AmirAdal <amir.adal@cobold.in>
2023-01-24 20:55:55 +0100

    feat: mentalDocument service method created

--- src/expression/expression.service.ts
+++ src/expression/expression.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, StreamableFile } from '@nestjs/common';
 
 import { MentalPayloadDto } from 'src/expression/dto/mental-payload.dto';
 import expression from 'src/lib/expression';
@@ -10,12 +10,51 @@ import multiply from 'src/lib/multiply';
 import { DividePayloadDto } from 'src/expression/dto/divide-payload.dto';
 import { DivideResponse } from 'src/expression/interface/divide-response.interface';
 import divide from 'src/lib/divide';
+import { MentalDocumentPayloadDto } from 'src/expression/dto/mental-documet-query.dto';
+import pdfGenerate from 'src/lib/pdf-generate';
+import { from, Observable, of, switchMap } from 'rxjs';
 
 @Injectable()
 export class ExpressionService {
   mental(payload: MentalPayloadDto): number[] {
     const { formula, terms, min, max, isBiggerMax } = payload;
     return expression(formula, terms, min, max, isBiggerMax);
+  }
+
+  mentalDocument(
+    payload: MentalDocumentPayloadDto,
+  ): Observable<StreamableFile> {
+    return from(
+      pdfGenerate(
+        {
+          formula: payload.formula,
+          termLength: payload.terms,
+          min: payload.min,
+          max: payload.max,
+        },
+        payload.pageCount,
+        {
+          hCount: payload.hCount,
+          vCount: payload.vCount,
+          isAutoHeight: payload.isAutoHeight,
+        },
+        {
+          byPage: payload.byPage,
+          byRows: payload.byRows,
+          isGrow: payload.isGrow,
+          isGrowByRows: payload.isGrowByRows,
+        },
+        payload.orientation,
+        payload.withRight,
+        payload.design,
+        payload.fileFormat,
+      ),
+    ).pipe(
+      switchMap((stream) => {
+        console.log({ stream });
+        return of(new StreamableFile(stream));
+      }),
+    );
   }
 
   multiply(payload: MultiplyPayloadDto): MultiplyResponse {
