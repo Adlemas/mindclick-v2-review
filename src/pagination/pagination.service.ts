e3e4a82ad16a49408c414ce4e155b59fd9aea76d
AmirAdal <amir.adal@cobold.in>
2023-01-27 22:38:59 +0100

    feat(pagination): make paginate method return paginated response

--- src/pagination/pagination.service.ts
+++ src/pagination/pagination.service.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { Query } from 'mongoose';
 import { PaginationQueryDto } from 'src/pagination/dto/pagination-query.dto';
+import { PaginatedResponse } from 'src/interface/paginated-response.interface';
+import { from, Observable, of, switchMap } from 'rxjs';
 
 @Injectable()
 export class PaginationService {
-  paginate<ResultType, DocType>(
+  paginate<ResultType extends Array<any>, DocType>(
     query: Query<ResultType, DocType>,
     dto: PaginationQueryDto,
-  ) {
+  ): Observable<PaginatedResponse<ResultType>> {
     const { page, size } = dto;
     const offset = (page - 1) * size;
-    return query.skip(offset).limit(size);
+    return from(query.skip(offset).limit(size).exec()).pipe(
+      switchMap((records) => {
+        return from(query.countDocuments().exec()).pipe(
+          switchMap((totalCount) => {
+            return of(this.buildPaginatedResponse(records, dto, totalCount));
+          }),
+        );
+      }),
+    );
+  }
+
+  buildPaginatedResponse<ResultType>(
+    records: ResultType,
+    dto: PaginationQueryDto,
+    totalCount: number,
+  ): PaginatedResponse<ResultType> {
+    const { page, size } = dto;
+    const totalPages = Math.ceil(totalCount / size);
+    return {
+      records,
+      totalPages,
+      totalCount,
+      page,
+    };
   }
 }
