fb829645f63ceb22eee9633b412f889516b35493
AmirAdal <amir.adal@cobold.in>
2023-01-27 21:55:58 +0100

    feat(tasks:controller): update task endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Inject,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { TasksService } from 'src/tasks/tasks.service';
 import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
 import { Roles } from 'src/users/decorator/roles.decorator';
 import { Role } from 'src/enum/role.enum';
 import { RolesGuard } from 'src/users/guard/roles.guard';
 import { CreateTaskDto } from 'src/tasks/dto/create-task.dto';
+import {
+  UpdateTaskDto,
+  UpdateTaskParamDto,
+} from 'src/tasks/dto/update-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -14,7 +27,27 @@ export class TasksController {
   @Roles(Role.TEACHER)
   @UseGuards(AccessTokenGuard, RolesGuard)
   @Post()
-  createTask(@Body() dto: CreateTaskDto) {
-    return this.tasksService.createTask(dto);
+  createTask(@Body() dto: CreateTaskDto, @Req() req) {
+    return this.tasksService.createTask(req.user, {
+      expiresAt: dto.expiresAt,
+      count: dto.count,
+      assignedTo: dto.assignedTo,
+      settings: dto.settings,
+      simulator: dto.simulator,
+    });
+  }
+
+  @Roles(Role.TEACHER)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Put(':id')
+  updateTask(
+    @Param() { id }: UpdateTaskParamDto,
+    @Body() dto: UpdateTaskDto,
+    @Req() req,
+  ) {
+    return this.tasksService.updateTask(req.user, id, {
+      expiresAt: dto.expiresAt,
+      count: dto.count,
+    });
   }
 }
