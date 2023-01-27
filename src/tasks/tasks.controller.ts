cb32ec5ddef25de8d06c40b01fb2f9feae3a01f1
AmirAdal <amir.adal@cobold.in>
2023-01-27 20:29:09 +0100

    feat(tasks:controller): protected by auth & role endpoint for creating new task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import { TasksService } from 'src/tasks/tasks.service';
+import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
+import { Roles } from 'src/users/decorator/roles.decorator';
+import { Role } from 'src/enum/role.enum';
+import { RolesGuard } from 'src/users/guard/roles.guard';
+import { CreateTaskDto } from 'src/tasks/dto/create-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  @Inject(TasksService)
+  private readonly tasksService: TasksService;
+
+  @Roles(Role.TEACHER)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Post()
+  createTask(@Body() dto: CreateTaskDto) {
+    return this.tasksService.createTask(dto);
+  }
+}
