a4c9356b52d1d80c374b525f145bd32c4d026b7c
AmirAdal <amir.adal@cobold.in>
2023-01-27 18:55:29 +0100

    feat(tasks): create task dto

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsEnum,
+  IsISO8601,
+  IsMongoId,
+  IsNumber,
+  IsObject,
+  IsString,
+} from 'class-validator';
+import { Simulator } from 'src/enum/simulator.enum';
+import { i18nValidationMessage } from 'nestjs-i18n';
+
+export class CreateTaskDto {
+  @IsEnum(Simulator, { message: i18nValidationMessage('validation.NOT_ENUM') })
+  simulator: Simulator;
+
+  @IsNumber({}, { message: i18nValidationMessage('validation.NOT_NUMBER') })
+  count: number;
+
+  // ISO date - expiresAt: string;
+  @IsISO8601(
+    { strict: true },
+    { message: i18nValidationMessage('validation.NOT_DATE') },
+  )
+  expiresAt: string;
+
+  @IsObject({ message: i18nValidationMessage('validation.NOT_OBJECT') })
+  settings: object;
+
+  @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
+  @IsMongoId({ message: i18nValidationMessage('validation.NOT_MONGO_ID') })
+  createdBy: string;
+
+  @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
+  @IsMongoId({ message: i18nValidationMessage('validation.NOT_MONGO_ID') })
+  assignedTo: string;
+}
