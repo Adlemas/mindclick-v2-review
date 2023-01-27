2822f642a4631d1b18760cbb66f0ddd0e81096db
AmirAdal <amir.adal@cobold.in>
2023-01-27 22:54:55 +0100

    feat(dto): use class-transformer in pagination-query dto

--- src/pagination/dto/pagination-query.dto.ts
+++ src/pagination/dto/pagination-query.dto.ts
@@ -1,13 +1,16 @@
 import { IsNumber, IsOptional, Min } from 'class-validator';
 import { i18nValidationMessage } from 'nestjs-i18n';
+import { Type } from 'class-transformer';
 
 export class PaginationQueryDto {
   @IsNumber({}, { message: i18nValidationMessage('validation.NOT_NUMBER') })
   @Min(1, { message: i18nValidationMessage('validation.MIN') })
+  @Type(() => Number)
   page: number;
 
   @IsNumber({}, { message: i18nValidationMessage('validation.NOT_NUMBER') })
   @Min(0, { message: i18nValidationMessage('validation.MIN') })
+  @Type(() => Number)
   @IsOptional()
   size?: number;
 }
