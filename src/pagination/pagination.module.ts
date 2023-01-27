304c8c42cb7df9409dc06770c60bcce8f1356b99
AmirAdal <amir.adal@cobold.in>
2023-01-27 22:11:16 +0100

    feat(pagination:module): make module global & export pagination service

--- src/pagination/pagination.module.ts
+++ src/pagination/pagination.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PaginationService } from './pagination.service';
 
+@Global()
 @Module({
   providers: [PaginationService],
+  exports: [PaginationService],
 })
 export class PaginationModule {}
