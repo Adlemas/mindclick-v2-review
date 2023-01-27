3d40e0796fd5be90a98f93d8f7efa5951928eb5a
AmirAdal <amir.adal@cobold.in>
2023-01-27 19:00:17 +0100

    feat(tasks): created task repo

---
+++ src/tasks/repository/task.repository.ts
@@ -0,0 +1,39 @@
+import { from, Observable } from 'rxjs';
+import { CreateTaskDto } from 'src/tasks/dto/create-task.dto';
+import { Task, TaskDocument } from 'src/schemas/task.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Schema } from 'mongoose';
+import { UpdateTaskDto } from 'src/tasks/dto/update-task.dto';
+
+export class TaskRepository {
+  @InjectModel(Task.name)
+  private readonly taskModel: Model<Task>;
+
+  create(dto: CreateTaskDto): Observable<TaskDocument> {
+    return from(this.taskModel.create(dto));
+  }
+
+  findById(id: Schema.Types.ObjectId): Observable<Task> {
+    return from(this.taskModel.findById(id).exec());
+  }
+
+  findOne(filter: Partial<Task>): Observable<Task> {
+    return from(this.taskModel.findOne(filter).exec());
+  }
+
+  findBy(filter: Partial<Task>): Observable<Array<Task>> {
+    return from(this.taskModel.find(filter).exec());
+  }
+
+  remove(id: Schema.Types.ObjectId): Observable<Task> {
+    return from(this.taskModel.findByIdAndDelete(id).exec());
+  }
+
+  update(id: Schema.Types.ObjectId, dto: UpdateTaskDto): Observable<Task> {
+    return from(
+      this.taskModel.findByIdAndUpdate(id, dto, {
+        new: true,
+      }),
+    );
+  }
+}
