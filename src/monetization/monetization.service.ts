2187ab7843d60f9f565e1a39d7a23d0b3eb8322d
AmirAdal <amir.adal@cobold.in>
2023-01-28 20:19:15 +0100

    feat(monetization:se): reward student method

--- src/monetization/monetization.service.ts
+++ src/monetization/monetization.service.ts
@@ -1,13 +1,18 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { Observable, of, switchMap } from 'rxjs';
 import { User } from 'src/schemas/user.schema';
 import { Monetization } from 'src/interface/monetization.interface';
 import { UserRepository } from 'src/users/repository/user.repository';
+import { Schema } from 'mongoose';
+import { LocaleService } from 'src/locale/locale.service';
 
 @Injectable()
 export class MonetizationService {
   @Inject(UserRepository)
   private readonly userRepository: UserRepository;
+
+  @Inject(LocaleService)
+  private readonly localeService: LocaleService;
 
   getMonetization(user: Observable<User>): Observable<Monetization> {
     return user.pipe(
@@ -53,6 +58,27 @@ export class MonetizationService {
             },
           })
           .pipe(switchMap((updatedUser) => of(updatedUser.monetization)));
+      }),
+    );
+  }
+
+  /**
+   * Not method for controller
+   * Reward user with specific amount of points & rate
+   * Should be called only for users with STUDENT role
+   */
+  reward(userId: Schema.Types.ObjectId, rate: number, points: number) {
+    return this.userRepository.findById(userId).pipe(
+      switchMap((user) => {
+        if (!user) {
+          throw new ForbiddenException(
+            this.localeService.translate('errors.user_not_found'),
+          );
+        }
+        return this.userRepository.updateById(userId, {
+          points: user.points + points,
+          rate: user.rate + rate,
+        });
       }),
     );
   }
