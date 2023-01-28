26227a59c6327857fcb545a30661a6e130ad07ce
AmirAdal <amir.adal@cobold.in>
2023-01-28 14:27:01 +0100

    feat(monetization:co): create, update & delete routes init

--- src/monetization/monetization.controller.ts
+++ src/monetization/monetization.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Post, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/users/decorator/roles.decorator';
+import { Role } from 'src/enum/role.enum';
+import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
+import { RolesGuard } from 'src/users/guard/roles.guard';
+import { CreateUpdateMonetizationDto } from 'src/monetization/dto/create-update-monetization.dto';
 
 @Controller('monetization')
-export class MonetizationController {}
+export class MonetizationController {
+  @Roles(Role.TEACHER)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Post()
+  createUpdateMonetization(@Body() dto: CreateUpdateMonetizationDto) {
+    return dto;
+  }
+
+  @Roles(Role.TEACHER)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Delete()
+  resetMonetization() {
+    return null;
+  }
+}
