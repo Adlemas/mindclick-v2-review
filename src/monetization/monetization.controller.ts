0e0cccf8410c0b508efc6137adf88833eb1aca71
AmirAdal <amir.adal@cobold.in>
2023-01-28 20:06:42 +0100

    feat(monetization:co): get monetization route

--- src/monetization/monetization.controller.ts
+++ src/monetization/monetization.controller.ts
@@ -1,12 +1,32 @@
-import { Body, Controller, Delete, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { Roles } from 'src/users/decorator/roles.decorator';
 import { Role } from 'src/enum/role.enum';
 import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
 import { RolesGuard } from 'src/users/guard/roles.guard';
 import { CreateUpdateMonetizationDto } from 'src/monetization/dto/create-update-monetization.dto';
+import { MonetizationService } from 'src/monetization/monetization.service';
 
 @Controller('monetization')
 export class MonetizationController {
+  @Inject(MonetizationService)
+  private readonly monetizationService: MonetizationService;
+
+  @Roles(Role.TEACHER)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Get()
+  getMonetization(@Req() req) {
+    return this.monetizationService.getMonetization(req.user);
+  }
+
   @Roles(Role.TEACHER)
   @UseGuards(AccessTokenGuard, RolesGuard)
   @Post()
