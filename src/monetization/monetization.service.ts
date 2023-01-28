8fad2633dc67858ed8121296b319047b7493053b
AmirAdal <amir.adal@cobold.in>
2023-01-28 20:07:23 +0100

    feat(monetization:se): safe get monetization settings method

--- src/monetization/monetization.service.ts
+++ src/monetization/monetization.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Observable, of, switchMap } from 'rxjs';
+import { User } from 'src/schemas/user.schema';
+import { Monetization } from 'src/interface/monetization.interface';
+import { UserRepository } from 'src/users/repository/user.repository';
 
 @Injectable()
-export class MonetizationService {}
+export class MonetizationService {
+  @Inject(UserRepository)
+  private readonly userRepository: UserRepository;
+
+  getMonetization(user: Observable<User>): Observable<Monetization> {
+    return user.pipe(
+      switchMap((user) => {
+        if (!user.monetization) {
+          return this.userRepository
+            .updateById(user._id, {
+              monetization: {
+                ignoreSimulators: [],
+                factor: 1,
+              },
+            })
+            .pipe(switchMap((updatedUser) => of(updatedUser.monetization)));
+        }
+        return of(user.monetization);
+      }),
+    );
+  }
+}
