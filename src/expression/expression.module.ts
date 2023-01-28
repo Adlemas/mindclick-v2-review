6d8a78ffdc1259dee86481b770f8add445b18d61
AmirAdal <amir.adal@cobold.in>
2023-01-28 21:47:26 +0100

    feat(expression): import monetization module for reward service

--- src/expression/expression.module.ts
+++ src/expression/expression.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExpressionController } from './expression.controller';
 import { ExpressionService } from './expression.service';
+import { MonetizationModule } from 'src/monetization/monetization.module';
 
 @Module({
+  imports: [MonetizationModule],
   controllers: [ExpressionController],
   providers: [ExpressionService],
 })
