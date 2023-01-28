2e8fe3f9fe22a6765164bfbb15a9848ed6be1e01
AmirAdal <amir.adal@cobold.in>
2023-01-28 20:06:55 +0100

    feat(monetization): import users module

--- src/monetization/monetization.module.ts
+++ src/monetization/monetization.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MonetizationController } from './monetization.controller';
 import { MonetizationService } from './monetization.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [MonetizationController],
   providers: [MonetizationService],
 })
