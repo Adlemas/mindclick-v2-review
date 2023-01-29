d92250cc68057f67009fcfeb911a921d8e6ff441
AmirAdal <amir.adal@cobold.in>
2023-01-29 10:26:18 +0100

    feat(reward:se): make reward method return update result

--- src/monetization/service/reward.service.ts
+++ src/monetization/service/reward.service.ts
@@ -2,7 +2,6 @@ import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { Schema } from 'mongoose';
 import { Reward } from 'src/monetization/interface/reward.interface';
 import { Observable, of, switchMap } from 'rxjs';
-import { User } from 'src/schemas/user.schema';
 import { Role } from 'src/enum/role.enum';
 import { UserRepository } from 'src/users/repository/user.repository';
 import { LocaleService } from 'src/locale/locale.service';
@@ -20,7 +19,7 @@ export class RewardService {
    * Reward user with specific amount of points & rate
    * Should be called only for users with STUDENT role
    */
-  reward(userId: Schema.Types.ObjectId, reward: Reward): Observable<User> {
+  reward(userId: Schema.Types.ObjectId, reward: Reward): Observable<Reward> {
     return this.userRepository.findById(userId).pipe(
       switchMap((user) => {
         if (!user) {
@@ -37,14 +36,26 @@ export class RewardService {
           switchMap((teacher) => {
             const { monetization } = teacher;
             if (monetization.ignoreSimulators.includes(reward.simulator)) {
-              return of(user);
+              return of({
+                points: 0,
+                rate: 0,
+              });
             }
             const points = reward.points * monetization.factor;
             const rate = reward.rate * monetization.factor;
-            return this.userRepository.updateById(user._id, {
-              points: user.points + points,
-              rate: user.rate + rate,
-            });
+            return this.userRepository
+              .updateById(user._id, {
+                points: user.points + points,
+                rate: user.rate + rate,
+              })
+              .pipe(
+                switchMap(() =>
+                  of({
+                    points,
+                    rate,
+                  }),
+                ),
+              );
           }),
         );
       }),
