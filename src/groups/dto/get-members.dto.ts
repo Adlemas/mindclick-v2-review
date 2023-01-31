48a368f50b8e7bfc72fb7fea2f47ace380040edb
AmirAdal <amir.adal@cobold.in>
2023-01-31 19:53:26 +0100

    feat: get members dto provided

---
+++ src/groups/dto/get-members.dto.ts
@@ -0,0 +1,9 @@
+import { PaginationQueryDto } from 'src/pagination/dto/pagination-query.dto';
+import { IsMongoId, IsString } from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
+
+export class GetMembersDto extends PaginationQueryDto {
+  @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
+  @IsMongoId({ message: i18nValidationMessage('validation.NOT_MONGO_ID') })
+  groupId?: string;
+}
