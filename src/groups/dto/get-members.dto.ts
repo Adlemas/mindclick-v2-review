31f8e9c531ef15a0f9d5360b07714a5ed75759b8
AmirAdal <amir.adal@cobold.in>
2023-01-31 20:05:06 +0100

    feat(dto): make groupId optional

--- src/groups/dto/get-members.dto.ts
+++ src/groups/dto/get-members.dto.ts
@@ -1,9 +1,10 @@
 import { PaginationQueryDto } from 'src/pagination/dto/pagination-query.dto';
-import { IsMongoId, IsString } from 'class-validator';
+import { IsMongoId, IsOptional, IsString } from 'class-validator';
 import { i18nValidationMessage } from 'nestjs-i18n';
 
 export class GetMembersDto extends PaginationQueryDto {
   @IsString({ message: i18nValidationMessage('validation.NOT_STRING') })
   @IsMongoId({ message: i18nValidationMessage('validation.NOT_MONGO_ID') })
+  @IsOptional()
   groupId?: string;
 }
