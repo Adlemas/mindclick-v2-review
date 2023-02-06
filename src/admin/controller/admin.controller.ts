891d1c10a50d5a12f39dff0d8ecf4bcd13e8643d
AmirAdal <amir.adal@cobold.in>
2023-02-06 15:38:13 +0100

    feat(admin:co): provided create user endpoint

--- src/admin/controller/admin.controller.ts
+++ src/admin/controller/admin.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Inject, Query, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AdminService } from 'src/admin/service/admin.service';
 import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
 import { AdminUserGuard } from 'src/users/guard/admin.guard';
 import { PaginationQueryDto } from 'src/pagination/dto/pagination-query.dto';
+import { CreateAdminDto } from 'src/admin/dto/create-admin.dto';
 
 @Controller('admin')
 export class AdminController {
@@ -13,5 +23,11 @@ export class AdminController {
   @Get('users')
   getUsers(@Req() req, @Query() paginationDto: PaginationQueryDto) {
     return this.adminService.getUsers(req.user, paginationDto);
+  }
+
+  @UseGuards(AccessTokenGuard, AdminUserGuard)
+  @Post('users')
+  createUser(@Req() req, @Body() dto: CreateAdminDto) {
+    return this.adminService.createUser(req.user, dto);
   }
 }
