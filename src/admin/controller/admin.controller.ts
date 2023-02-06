27edad76f59d6954d1246cf3c34de18957e7313e
AmirAdal <amir.adal@cobold.in>
2023-02-06 10:42:08 +0100

    feat: provided get users endpoint for admin module

--- src/admin/controller/admin.controller.ts
+++ src/admin/controller/admin.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject, Query, Req, UseGuards } from '@nestjs/common';
+import { AdminService } from 'src/admin/service/admin.service';
+import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
+import { AdminUserGuard } from 'src/users/guard/admin.guard';
+import { PaginationQueryDto } from 'src/pagination/dto/pagination-query.dto';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  @Inject(AdminService)
+  private readonly adminService: AdminService;
+
+  @UseGuards(AccessTokenGuard, AdminUserGuard)
+  @Get('users')
+  getUsers(@Req() req, @Query() paginationDto: PaginationQueryDto) {
+    return this.adminService.getUsers(req.user, paginationDto);
+  }
+}
