6b24bbaa269b0eb37fc8ca99ae4063b02802edf7
AmirAdal <amir.adal@cobold.in>
2023-02-06 10:42:21 +0100

    feat: import users module in admin

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './service/admin.service';
 import { AdminController } from './controller/admin.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   providers: [AdminService],
   controllers: [AdminController],
 })
