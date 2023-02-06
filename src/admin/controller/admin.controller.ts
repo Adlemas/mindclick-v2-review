f602bef38667270cdfb7c308796e441fc1a3a528
AmirAdal <amir.adal@cobold.in>
2023-02-06 16:04:44 +0100

    feat(admin:co): provided update user endpoint

--- src/admin/controller/admin.controller.ts
+++ src/admin/controller/admin.controller.ts
@@ -3,7 +3,9 @@ import {
   Controller,
   Get,
   Inject,
+  Param,
   Post,
+  Put,
   Query,
   Req,
   UseGuards,
@@ -13,6 +15,10 @@ import { AccessTokenGuard } from 'src/auth/guard/access-token.guard';
 import { AdminUserGuard } from 'src/users/guard/admin.guard';
 import { PaginationQueryDto } from 'src/pagination/dto/pagination-query.dto';
 import { CreateAdminDto } from 'src/admin/dto/create-admin.dto';
+import {
+  UpdateAdminDto,
+  UpdateAdminParamsDto,
+} from 'src/admin/dto/update-admin.dto';
 
 @Controller('admin')
 export class AdminController {
@@ -29,5 +35,15 @@ export class AdminController {
   @Post('users')
   createUser(@Req() req, @Body() dto: CreateAdminDto) {
     return this.adminService.createUser(req.user, dto);
+  }
+
+  @UseGuards(AccessTokenGuard, AdminUserGuard)
+  @Put('users/:id')
+  updateUser(
+    @Req() req,
+    @Param() { id }: UpdateAdminParamsDto,
+    @Body() dto: UpdateAdminDto,
+  ) {
+    return this.adminService.updateUser(req.user, id, dto);
   }
 }
