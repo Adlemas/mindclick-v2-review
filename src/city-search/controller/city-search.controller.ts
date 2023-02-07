1bc7be7f83aee0530750b4e3fa4f5dcec43450ea
AmirAdal <amir.adal@cobold.in>
2023-02-07 15:16:38 +0100

    feat: provided method for getting cities with query search param

--- src/city-search/controller/city-search.controller.ts
+++ src/city-search/controller/city-search.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { CitySearchService } from 'src/city-search/service/city-search.service';
+import { SearchCitiesDto } from 'src/city-search/dto/search-cities.dto';
 
 @Controller('city-search')
-export class CitySearchController {}
+export class CitySearchController {
+  @Inject(CitySearchService)
+  private readonly citySearchService: CitySearchService;
+
+  @Get()
+  getCities(@Query() searchDto: SearchCitiesDto) {
+    return this.citySearchService.findCities({
+      limit: searchDto.limit,
+      query: searchDto.query,
+    });
+  }
+}
