fc0b2c5694ae3ff1606d7837bb94d2044f896b26
AmirAdal <amir.adal@cobold.in>
2023-02-07 15:08:09 +0100

    feat: import mongoose "city" feature

--- src/city-search/city-search.module.ts
+++ src/city-search/city-search.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CitySearchController } from './controller/city-search.controller';
 import { CitySearchService } from './service/city-search.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { City, CitySchema } from 'src/schemas/city.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: City.name,
+        schema: CitySchema,
+      },
+    ]),
+  ],
   controllers: [CitySearchController],
   providers: [CitySearchService],
 })
