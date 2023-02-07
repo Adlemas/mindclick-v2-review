51eeb65dfe32b8ed7f91208357e7e51888fc26b3
AmirAdal <amir.adal@cobold.in>
2023-02-07 15:08:21 +0100

    feat: provided method for searching cities

--- src/city-search/service/city-search.service.ts
+++ src/city-search/service/city-search.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { City } from 'src/schemas/city.schema';
+import { Model } from 'mongoose';
+import { SearchCitiesDto } from 'src/city-search/dto/search-cities.dto';
+import { from, Observable } from 'rxjs';
 
 @Injectable()
-export class CitySearchService {}
+export class CitySearchService {
+  @InjectModel(City.name)
+  private readonly cityModel: Model<City>;
+
+  findCities(dto: SearchCitiesDto): Observable<Array<City>> {
+    return from(
+      this.cityModel
+        .find({
+          city: {
+            $regex: dto.query,
+            $options: 'gi',
+          },
+        })
+        .limit(dto.limit ?? 20)
+        .exec(),
+    );
+  }
+}
